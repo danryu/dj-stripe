16bec138679a261a60191e5df01eae307f7a94a4
dan garton <dan.garton@gmail.com>
2018-06-04 19:34:57 +0100

    import old views and templates for easy setup

--- djstripe/forms.py
+++ djstripe/forms.py
@@ -7,10 +7,16 @@
 .. moduleauthor:: Daniel Greenfeld (@pydanny)
 
 """
+
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from django import forms
+from .models import Plan
 
+class PlanForm(forms.Form):
+    """A form used when creating a Plan."""
+
+    plan = forms.ModelChoiceField(queryset=Plan.objects.all())
 
 class CancelSubscriptionForm(forms.Form):
     """A form used when canceling a Subscription."""
